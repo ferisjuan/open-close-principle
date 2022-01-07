e12f46a625912a0df74333c796de986b8bae687b
Juan Feris <juan@Juans-MacBook-Pro.local>
2022-01-07 16:17:22 -0500

    feat(ci): prettier

--- src/models/employee.ts
+++ src/models/employee.ts
@@ -1,11 +1,17 @@
-export class EmployeeModel{
+export class EmployeeModel {
     public FirstName: string
     public LastName: string
     public Email: string
     public isManager: boolean
     public isExecutive: boolean
 
-    constructor(FirstName= '', LastName = '', Email = '', isManager = false, isExecutive = false) {
+    constructor(
+        FirstName = '',
+        LastName = '',
+        Email = '',
+        isManager = false,
+        isExecutive = false
+    ) {
         this.FirstName = FirstName
         this.LastName = LastName
         this.Email = Email
